b29461a4bdbb0873abd42be542e64b4aa04e400d
songtaogui <songtaogui@sina.com>
2023-06-24 19:33:07 +0800

    23-06-24 19:33

--- deploy.sh
+++ deploy.sh
@@ -32,7 +32,9 @@ USAGE:
 
 OPTIONS: ([R]:required  [O]:optional)
     -h, --help                          show help and exit.
-    -b, --builder                       deploy the builder instead of the blog site.
+    -b, --builder                       deploy the builder instead of the blog site, 
+                                        default: only deploy the blog repos (__site dir)
+    --all                               deploy both the builder and the blog repos
     -f, --force                         force git push 
     -m, --commit  <str>                 github commit info, default: 'date'
     -d, --dryrun                        skip the github deploy step, just output the cmd
@@ -56,6 +58,7 @@ check_sftw_path staticrypt
 export template="_layout/staticrypt_template.html"
 export commit=$(date +'%y-%m-%d %H:%M')
 export builder=FALSE
+export blog=TRUE
 export force=FALSE
 export dryrun=FALSE
 # parse args
@@ -68,6 +71,11 @@ while [[ $# > 0 ]]; do
         ;;
         -b|--builder)
             builder=TRUE
+            shift 1
+        ;;
+        --all)
+            builder=TRUE
+            blog=TRUE
             shift 1
         ;;
         -m|--commit)
@@ -122,153 +130,155 @@ if [ "$builder" == "TRUE" ];then
         -------------------------------
         "
     fi
-else
-gst_log "Deploying to my blog site $(git branch -vv)..."
-gst_rcd "Check ref.bib and format ..."
-# check if the ref.bib has been updated with md5
-if ! md5sum -c _assets/citedb/ref.bib.md5 1>/dev/null 2>&1; then
-    gst_rcd "ref.bib has been updated, re-format it ..."
-    # remove the file and abstract track from bib
-    mv _assets/citedb/ref.bib _assets/citedb/ref.bib.bak &&\
-    perl -pe '
-        s/^\s*file = \{.*\},\s*$//g;
-        s/^\s*abstract = \{.*\},\s*$//g;
-    ' _assets/citedb/ref.bib.bak |\
-    perl -lane '
-        # keep only intact records:
-        # records with title, author|editor, year, journal|publisher
-        if(/^@/){
-            $cur_key = $F[0];
-            $cur_key =~ s/@.*?\{(.*),$/$1/;
-            @cur_record = ();
-            $in_record = 1;
-            $abstract = 0;
-            $valid_score = 0;
-            push @cur_record, $_;
-        }elsif($in_record){
-            # skip abstract record
-            if(/^\s*abstract = /){
-                $abstract = 1;
-            }elsif(/^\s*\}/ and $abstract == 1){
+fi # end of builder
+
+if [ "$blog" == "TRUE" ];then
+    gst_log "Deploying to my blog site $(git branch -vv)..."
+    gst_rcd "Check ref.bib and format ..."
+    # check if the ref.bib has been updated with md5
+    if ! md5sum -c _assets/citedb/ref.bib.md5 1>/dev/null 2>&1; then
+        gst_rcd "ref.bib has been updated, re-format it ..."
+        # remove the file and abstract track from bib
+        mv _assets/citedb/ref.bib _assets/citedb/ref.bib.bak &&\
+        perl -pe '
+            s/^\s*file = \{.*\},\s*$//g;
+            s/^\s*abstract = \{.*\},\s*$//g;
+        ' _assets/citedb/ref.bib.bak |\
+        perl -lane '
+            # keep only intact records:
+            # records with title, author|editor, year, journal|publisher
+            if(/^@/){
+                $cur_key = $F[0];
+                $cur_key =~ s/@.*?\{(.*),$/$1/;
+                @cur_record = ();
+                $in_record = 1;
                 $abstract = 0;
-                next;
-            }
-            push @cur_record, $_ if $abstract == 0;
-            if(/^}/){
-                $in_record = 0;
-                foreach $line (@cur_record){
-                    if( $line =~ /^\s*title = / or
-                        $line =~ /^\s*(author|editor) = / or
-                        $line =~ /^\s*year/ or
-                        $line =~ /^\s*(journal|publisher) = /)
-                    {
-                        $valid_score ++;
+                $valid_score = 0;
+                push @cur_record, $_;
+            }elsif($in_record){
+                # skip abstract record
+                if(/^\s*abstract = /){
+                    $abstract = 1;
+                }elsif(/^\s*\}/ and $abstract == 1){
+                    $abstract = 0;
+                    next;
+                }
+                push @cur_record, $_ if $abstract == 0;
+                if(/^}/){
+                    $in_record = 0;
+                    foreach $line (@cur_record){
+                        if( $line =~ /^\s*title = / or
+                            $line =~ /^\s*(author|editor) = / or
+                            $line =~ /^\s*year/ or
+                            $line =~ /^\s*(journal|publisher) = /)
+                        {
+                            $valid_score ++;
+                        }
+                    }
+                    if($valid_score >= 4){
+                        print join("\n", @cur_record);
+                    }else{
+                        print STDERR "\033[35m[WARNING]\033[0m -->","Skip invalid record:$cur_key";
                     }
                 }
-                if($valid_score >= 4){
-                    print join("\n", @cur_record);
-                }else{
-                    print STDERR "\033[35m[WARNING]\033[0m -->","Skip invalid record:$cur_key";
-                }
             }
-        }
-    ' > _assets/citedb/ref.bib &&\
-    md5sum _assets/citedb/ref.bib > _assets/citedb/ref.bib.md5
-    if [ $? -ne 0 ];then gst_err "md5sum ref.bib failed: Non-zero exit"; exit 1;fi
-else
-    gst_rcd "ref.bib has not been updated, skip re-format ..."
-fi
-
-gst_log "Encrypting private files ..."
-
-find . -name "*.md" | grep -v -P "^\.\/[_\.]" | while read mdfile
-do
-    # check if name with space
-    if [[ "${mdfile}" == *" "* ]]; then
-        gst_err "file [ $mdfile ] should NOT contain whitespace in the name !"
-        exit 1
+        ' > _assets/citedb/ref.bib &&\
+        md5sum _assets/citedb/ref.bib > _assets/citedb/ref.bib.md5
+        if [ $? -ne 0 ];then gst_err "md5sum ref.bib failed: Non-zero exit"; exit 1;fi
+    else
+        gst_rcd "ref.bib has not been updated, skip re-format ..."
     fi
-    # check if has encrypt code
-    encode=$(grep -o -P "^encrypt = ([123])" $mdfile)
-    if [[ -n "${encode}" ]]; then
-        PSWD=""
-        CRPYT="FALSE"
-        if [[ "$encode" == "encrypt = 1" ]];then
-            PSWD=$FRANKLINPSWD1
-        elif [[ "$encode" == "encrypt = 2" ]];then
-            PSWD=$FRANKLINPSWD2
-        elif [[ "$encode" == "encrypt = 3" ]];then
-            PSWD=$FRANKLINPSWD3
-        fi
-        if [[ -z "${PSWD}" ]]; then
-            gst_err "no password detected! Please check the env!"
+
+    gst_rcd "Encrypting private files ..."
+
+    find . -name "*.md" | grep -v -P "^\.\/[_\.]" | while read mdfile
+    do
+        # check if name with space
+        if [[ "${mdfile}" == *" "* ]]; then
+            gst_err "file [ $mdfile ] should NOT contain whitespace in the name !"
             exit 1
         fi
-        # get the html file:
-        htmlfile=$(echo "$mdfile" | perl -pe '
-            s/^\.\//\.\/__site\//;
-            if(/index.md$/){
-                s/\.md$/\.html/;
-            }else{
-                s/\.md$/\/index.html/;
-            }
-            ')
-        check_files_exists $htmlfile
-        # check if htmlfile is already encrypted
-        head $htmlfile | grep "staticrypt" 1>/dev/null 2>&1
-        if [ $? -ne 0 ];then
-            # un-encrypted
-            CRPYT="TRUE"
+        # check if has encrypt code
+        encode=$(grep -o -P "^encrypt = ([123])" $mdfile)
+        if [[ -n "${encode}" ]]; then
+            PSWD=""
+            CRPYT="FALSE"
+            if [[ "$encode" == "encrypt = 1" ]];then
+                PSWD=$FRANKLINPSWD1
+            elif [[ "$encode" == "encrypt = 2" ]];then
+                PSWD=$FRANKLINPSWD2
+            elif [[ "$encode" == "encrypt = 3" ]];then
+                PSWD=$FRANKLINPSWD3
+            fi
+            if [[ -z "${PSWD}" ]]; then
+                gst_err "no password detected! Please check the env!"
+                exit 1
+            fi
+            # get the html file:
+            htmlfile=$(echo "$mdfile" | perl -pe '
+                s/^\.\//\.\/__site\//;
+                if(/index.md$/){
+                    s/\.md$/\.html/;
+                }else{
+                    s/\.md$/\/index.html/;
+                }
+                ')
+            check_files_exists $htmlfile
+            # check if htmlfile is already encrypted
+            head $htmlfile | grep "staticrypt" 1>/dev/null 2>&1
+            if [ $? -ne 0 ];then
+                # un-encrypted
+                CRPYT="TRUE"
+            else
+                # already encrypted
+                gst_warn "skip $htmlfile"
+            fi
+            # encrypt it
+            if [[ $CRPYT == "TRUE" ]];then
+                gst_rcd "Encrypt $htmlfile ..."
+                staticrypt --password $PSWD \
+                    --template $template \
+                    --short \
+                    --template-remember "Remember me (30 days)"\
+                    --template-button "DECRYPT" \
+                    --template-color-primary "#81B8F7" \
+                    --template-color-secondary "#DAEFF8" \
+                    --template-error "Bad password!" \
+                    --template-placeholder "Password" \
+                    --template-title "Show me what you got" \
+                    --remember 30 \
+                    $htmlfile &&\
+                mv -f encrypted/$(basename $htmlfile) $htmlfile &&\
+                rm -rf encrypted
+                if [ $? -ne 0 ];then gst_err "staticrypt $htmlfile failed: Non-zero exit"; exit 1;fi
+            fi
+        fi
+    done
+    if [ $? -ne 0 ];then gst_err "Presteps failed: Non-zero exit"; exit 1;fi
+
+    gst_rcd "Deploy on github with commit [ $commit ] ..."
+
+    if [[ $dryrun != "TRUE" ]];then
+        cd __site || exit 1
+        git add . &&\
+        git commit -m "$commit" &&\
+        if [ "$force" == "TRUE" ];then
+            git push -u origin master -f
         else
-            # already encrypted
-            gst_warn "skip $htmlfile"
+            git push origin master
         fi
-        # encrypt it
-        if [[ $CRPYT == "TRUE" ]];then
-            gst_rcd "Encrypt $htmlfile ..."
-            staticrypt --password $PSWD \
-                --template $template \
-                --short \
-                --template-remember "Remember me (30 days)"\
-                --template-button "DECRYPT" \
-                --template-color-primary "#81B8F7" \
-                --template-color-secondary "#DAEFF8" \
-                --template-error "Bad password!" \
-                --template-placeholder "Password" \
-                --template-title "Show me what you got" \
-                --remember 30 \
-                $htmlfile &&\
-            mv -f encrypted/$(basename $htmlfile) $htmlfile &&\
-            rm -rf encrypted
-            if [ $? -ne 0 ];then gst_err "staticrypt $htmlfile failed: Non-zero exit"; exit 1;fi
-        fi
-    fi
-done
-if [ $? -ne 0 ];then gst_err "Presteps failed: Non-zero exit"; exit 1;fi
-
-gst_log "Deploy on github with commit [ $commit ] ..."
-
-
-if [[ $dryrun != "TRUE" ]];then
-    cd __site || exit 1
-    git add . &&\
-    git commit -m "$commit" &&\
-    if [ "$force" == "TRUE" ];then
-        git push -u origin master -f
     else
+    gst_warn "Dryrun commands:
+    -------------------------------
+        cd __site &&\\
+        git add . &&\\
+        git commit -m \"$commit\" &&\\
         git push origin master
-    fi
-else
-   gst_warn "Dryrun commands:
--------------------------------
-    cd __site &&\\
-    git add . &&\\
-    git commit -m \"$commit\" &&\\
-    git push origin master
--------------------------------
-   "
-fi # end of if dryrun
-fi # end of if builder
+    -------------------------------
+    "
+    fi # end of if dryrun
+fi # end of blog
+
 if [ $? -ne 0 ];then gst_err "Deploy failed: Non-zero exit"; exit 1;fi
 
 gst_log "All Done !"
