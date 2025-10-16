6c956ec3eae755078a870481d547f06b48a336ae
songtaogui <songtaogui@sina.com>
2025-10-16 08:49:06 +0800

    25-10-16 08:48

---
+++ utils.sync-conflict-20251016-084437-PD7KHB5.jl
@@ -0,0 +1,12 @@
+using BibParser
+using FranklinUtils
+using Dates
+
+const AUTHOR = "Gui Songtao"
+
+include("src/WebIO.jl")
+include("src/Common.jl")
+include("src/Academic.jl")
+include("src/Citation.jl")
+include("src/Slides.jl")
+
